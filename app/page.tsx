d588f0eb33e1c8efef951d71e8de909409428caa
okmtyuta <okmtyuta@gmail.com>
2023-05-02 23:07:35 +0900

    labeling

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,18 @@
 
 import { Retex } from 'math-okmtyuta/components/ReTex/Retex'
 
-const script = String.raw`
+export default function Home() {
+  const script = String.raw`
   確率変数$X$が正規分布$N(\mu, \sigma^2)$に従うとする。この時$X$の一次積率$\alpha_1$は
   \begin{align}
     \alpha_1 = \int_{-\infty}^\infty x \frac{1}{2\pi} \exp\left( -\frac{(x-\mu)^2}{2\sigma^2} \right) dx
   \end{align}
   で与えられる。これを計算して期待値$\mu$を得る。
 `
-
-export default function Home() {
   return (
     <main>
-      <div>
-        以下のように数式が表示できる。
-      </div>
-      <Retex text={script} />
+      <div>以下のように数式が表示できる。</div>
+      {/* <Retex text={script} /> */}
     </main>
   )
 }
